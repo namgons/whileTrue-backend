0ed4638f601a0d68590f8867a2fbbffdb17e0175
Lee, Nam Gon <nglee.dev@gmail.com>
2024-02-14 20:50:22 +0900

    [feat] 데이터베이스 조회시 ID 없는 경우 예외 발생

--- src/main/java/com/whiletruebackend/domain/Problem/service/ProblemServiceImpl.java
+++ src/main/java/com/whiletruebackend/domain/Problem/service/ProblemServiceImpl.java
@@ -5,6 +5,7 @@ import com.whiletruebackend.domain.Problem.dto.request.ProblemRequestDto;
 import com.whiletruebackend.domain.Problem.dto.response.ProblemExistenceResponseDto;
 import com.whiletruebackend.domain.Problem.dto.response.ProblemListResponseDto;
 import com.whiletruebackend.domain.Problem.vo.Problem;
+import com.whiletruebackend.global.error.exception.MemberDatabaseIdNotFoundException;
 import com.whiletruebackend.global.notion.service.NotionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,20 +20,34 @@ public class ProblemServiceImpl implements ProblemService {
 
     @Override
     public ProblemListResponseDto getProblemList(Member member) {
-        List<Problem> problemList = notionService.getAllProblemList(member.getNotionApiKey(), member.getNotionSpace().getDatabaseId());
+        String databaseId = member.getNotionSpace().getDatabaseId();
+        isDatabaseIdExists(databaseId);
+
+        List<Problem> problemList = notionService.getAllProblemList(member.getNotionApiKey(), databaseId);
         return ProblemListResponseDto.from(problemList);
     }
 
     @Override
     public ProblemExistenceResponseDto checkIfProblemExits(Member member, ProblemRequestDto problemRequestDto) {
-        boolean isExists = notionService.isProblemExists(member.getNotionApiKey(), member.getNotionSpace().getDatabaseId(),
-                                                         problemRequestDto.getProblem());
+        String databaseId = member.getNotionSpace().getDatabaseId();
+        isDatabaseIdExists(databaseId);
+
+        boolean isExists = notionService.isProblemExists(member.getNotionApiKey(), databaseId, problemRequestDto.getProblem());
 
         return ProblemExistenceResponseDto.from(isExists);
     }
 
     @Override
     public void insertNewProblem(Member member, ProblemRequestDto problemRequestDto) {
-        notionService.insertNewProblem(member.getNotionApiKey(), member.getNotionSpace().getDatabaseId(), problemRequestDto.getProblem());
+        String databaseId = member.getNotionSpace().getDatabaseId();
+        isDatabaseIdExists(databaseId);
+
+        notionService.insertNewProblem(member.getNotionApiKey(), databaseId, problemRequestDto.getProblem());
+    }
+
+    private void isDatabaseIdExists(String databaseId) {
+        if (databaseId == null) {
+            throw MemberDatabaseIdNotFoundException.EXCEPTION;
+        }
     }
 }
