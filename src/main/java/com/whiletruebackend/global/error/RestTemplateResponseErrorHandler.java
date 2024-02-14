b88c61f875786b4b768efb85a2d21f2730071fb6
Lee, Nam Gon <nglee.dev@gmail.com>
2024-02-14 17:53:59 +0900

    [fix] Notion 에러시에 status 출력

--- src/main/java/com/whiletruebackend/global/error/RestTemplateResponseErrorHandler.java
+++ src/main/java/com/whiletruebackend/global/error/RestTemplateResponseErrorHandler.java
@@ -5,12 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 
+import java.io.IOException;
+
 @Slf4j
 public class RestTemplateResponseErrorHandler extends DefaultResponseErrorHandler {
 
     @Override
-    public void handleError(ClientHttpResponse response) {
-        log.error("[Notion Error]");
+    public void handleError(ClientHttpResponse response) throws IOException {
+        log.error("[Notion Error] " + response.getStatusText());
         throw NotionConnectionErrorException.EXCEPTION;
     }
 }
