a63858a65e7e3e6b0ad34ae96baa85bde597e771
Lee, Nam Gon <nglee.dev@gmail.com>
2024-02-15 11:37:58 +0900

    [fix] 패턴으로 CORS 허용

--- src/main/java/com/whiletruebackend/global/config/WebConfig.java
+++ src/main/java/com/whiletruebackend/global/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigin)
+                .allowedOriginPatterns(allowedOrigin)
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
