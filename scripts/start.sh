2e9acebab750e054160ba3de6697a22726910271
Lee, Nam Gon <nglee.dev@gmail.com>
2024-02-27 12:11:27 +0900

    [chore] 배포 그룹 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,5 +5,8 @@ ROOT_PATH="/home/ubuntu/app"
 CONTAINER="app_container"
 IMAGE="app_image"
 
+NETWORK="custom-network"
+
 docker build -t "$IMAGE" "$ROOT_PATH"
-docker run -dp 80:80 --name "$CONTAINER" "$IMAGE"
+docker run -dp 443:80 --name "$CONTAINER" "$IMAGE"
+docker network connect "$NETWORK" "$CONTAINER"
